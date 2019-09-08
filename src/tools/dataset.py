f3ce506171fd46576ea5fb9923b19ea2f6c23c7a
Gori89 <gori897@gmail.com>
2019-09-08 13:30:25 +0200

    funciones de recuperacion de datos

--- src/tools/dataset.py
+++ src/tools/dataset.py
@@ -3,8 +3,23 @@ import pandas as pd
 
 period=""
 
+def createDF(date, delta):
+    #formated in the input
+    #setDate(date)
+    delta=setDelta(delta)
+    #creat the dataframe with the necesary years 
+    df=getDataset(date,delta)
+    print("Se crea el dataframe")
+    #put the colum "date" as the index
+    df=dateToIndex(df)
+    #group the df by datetime taking the mean of all the stations
+    df=meanbyDate(df)
+    print("Se formatea el dataframe")
+    df=filterDFbyDate(df,date,delta)
+    return df
+
 def setDate(date):
-    #ToDo:validar que la fecha es correcta
+    #ToDo:validate format
     if len(date)>=10:
         day=int(date[8:10])
         setPeriod("D")
@@ -29,11 +44,11 @@ def getPeriod():
     
     
 def setDelta(delta):
-    global period
-    if period=="D":
-        return datetime.timedelta(days=delta)
+    global period      
     if period=="M":
         return datetime.timedelta(days=delta*30)
+    else:
+        return datetime.timedelta(days=delta)
 
 
 def getDataset(date1,date2=0):
@@ -58,9 +73,15 @@ def getDataset(date1,date2=0):
 def dateToIndex(df):
     df["datetime"] = pd.to_datetime(df["date"])
     df = df.set_index('datetime')
-    return df.drop(columns=["date"])
+    #df = df.drop(columns=["date"])
+    return df
 
 
 def meanbyDate(df):
     return df.groupby(['datetime']).mean()
 
+def filterDFbyDate(df,date,delta):
+    filter_before=df.index.date>=(date-delta).date()
+    filter_after=df.index.date<=(date+delta).date()
+
+    return df[filter_before & filter_after]
